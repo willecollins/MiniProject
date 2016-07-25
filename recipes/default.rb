74a0b879b2ca98c7ddbb9d46c5d8b9f85418490c
willecollins <willc@designtime.com>
2016-07-25 12:38:38 -0700

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,6 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 
-template '/usr/share/nginx/html/index.html' do
+template '/etc/nginx/sites-enabled/index.html' do
   source 'index.html.erb'
 end
