5e3ab089984c0db1a097ae3bb43aabb8601c662c
willecollins <willc@designtime.com>
2016-07-25 13:40:36 -0700

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,8 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
-
+sudo yum -y install httpd
+sudo service httpd start
 
 template '/etc/nginx/sites-enabled/index.html' do
   source 'index.html.erb'
