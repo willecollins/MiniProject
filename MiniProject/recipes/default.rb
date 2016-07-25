03ee9ff802df98aef18d79473ae171553e189c8d
willecollins <willc@designtime.com>
2016-07-25 15:01:48 -0700

    Update default.rb

--- MiniProject/recipes/default.rb
+++ MiniProject/recipes/default.rb
@@ -4,4 +4,14 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-mkdir testfolder
+sudo yum -y install httpd
+
+sudo service httpd start
+
+
+
+template '/var/www/html/index.html' do
+
+  source 'index.html.erb'
+
+end
