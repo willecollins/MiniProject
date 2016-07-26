71869afa16f8a959c39786802041d57b1b2b6bc6
DesigntimeSoftware <willcollins@outlook.com>
2016-07-26 00:07:18 -0700

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,13 @@
 sudo yum -y install httpd
 sudo service httpd start
 
-template '/var/www/html/index.html' do
-  source 'index.html.erb'
+#template '/var/www/html/index.html' do
+#  source 'index.html.erb'
+#end
+file '/var/www/html/index.html' do
+  content '<html>
+  <body>
+    <h1>Automation for the People/h1>
+  </body>
+</html>'
 end
